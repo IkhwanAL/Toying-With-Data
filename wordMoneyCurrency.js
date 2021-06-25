e3f2e91c58671974d0e600d45cf8529122c8751c
Ikhwan A Latif <56556145+IkhwanAL@users.noreply.github.com>
2021-06-25 16:54:01 +0700

    Update wordMoneyCurrency.js

--- wordMoneyCurrency.js
+++ wordMoneyCurrency.js
@@ -1,16 +1,16 @@
 /**
  * Create By Simplefied Every Function 
  * Like Determine it 'Ratusan' or 'Puluhan', 'Satuan'
- * Every Number Only have 3 number
+ * Every Array Only have 3 number ('Ratusan', 'Puluhan', And 'Satuan')
  * 
  * e.g:
  * 14859343
  * there are 8 number in there, divided into 3 array inside an array
- * and each array contain below equels hunderds
- *  e.g:
- *  [[1,4],[8,5,9],[3,4,3]]
+ * and each array contain below or equels hunderds
+ * e.g:
+ * [[1,4],[8,5,9],[3,4,3]] => MultiDimensional Array
  * with these we can easy manipulate the number and decided which
- * is hundeds, which is Dozen and Which is Unit Number
+ * is hundreds, which is Dozen and Which is Unit Number
  * After That
  * we can determine which is billion, Million, and Thousand
  * by looking at outside inner Array
@@ -31,9 +31,9 @@ const satuan = {
     '1000': 'Seribu'
 };
 
-let startIndexPosition = 0;
+let startIndexPosition = 0; // Index for outside array
 
-const keysNumber = Object.keys(satuan);
+const keysNumber = Object.keys(satuan); 
 
 function readRatusan(arr){
     if(typeof arr === 'undefined' || typeof arr !== 'object'){
@@ -45,22 +45,26 @@ function readRatusan(arr){
     }
 
     let word = '';
-    if(arr.length - 1 < 2){
+    if(arr.length <= 2){
         word = readPuluhan(arr);
         return word
     }
     
-    if(arr[0] == 0){
-		newArr = [arr[1], arr[2]]
-        word = readPuluhan(arr);
-        return word;
-    }
+    // if(arr.length == 2){
+	// 	newArr = [arr[1], arr[2]]
+    //     word = readPuluhan(newArr);
+    //     return word;
+    // }
 
-    if(arr[0] == 1){
+    if(arr[0] == 1 && arr[1] == 0 && arr[2] == 0){
         word += satuan[100] + ' ';
-    }else{
+    }
+	if(arr[0] != 0){
         word += readSatuan([], arr[0]) + 'Ratus ';
     }
+	if(arr[1] == 0 && arr[2] == 0){
+		return word
+	}
     
     word += readPuluhan([arr[1], arr[2]]);
     // word += readSatuan([], arr[2]);
@@ -72,19 +76,19 @@ function readRatusan(arr){
  * it can be Complicated When reach 
  * eleven to nineteen number And Check For Dozens Number
  */
-function readPuluhan(arr = [], value){
-    if(arr.length - 1 < 1){
+function readPuluhan(arr = []){
+    if(arr.length <= 1){
         let word = readSatuan(arr)
         return word;
     }
-    if(arr[1] == 0){	
-        let word = readSatuan(arr);
-        return word;
-    }
+    // if(arr[1] == 0){	
+    //     let word = readSatuan(arr);
+    //     return word;
+    // }
     
     let word = '';
-	let i= 0;
 	if(arr.length == 3){
+		let i = 1;
 		while(i < 2){
 			if(arr[i] == 0){
 				i++;
@@ -112,6 +116,8 @@ function readPuluhan(arr = [], value){
 		}  
 	}
 	if(arr.length == 2){
+		console.log('im');
+		let i = 0;
 		while (i < 1){
 			if(arr[i] == 1 && arr[i + 1] == 0){
 				word += satuan[10] + ' ';
@@ -162,7 +168,6 @@ function readSatuan(arr = [], value = 0){
     }
     if(value != 0){
         for(let i in satuan){
-            // console.log(`${satuan[i]} => ${i}`)
             if(keysNumber[i - 1] == value){
                 word += `${satuan[i]} `;
             }
@@ -171,6 +176,16 @@ function readSatuan(arr = [], value = 0){
     // console.log(word);
     return word;
     
+}
+
+function convertTrilyun(arr, position) {
+	if(typeof position !== 'number'){
+		position = +position;
+	}
+
+	let word = readRatusan(arr[position]) + 'Trilyun ';
+	startIndexPosition++;
+	return word;
 }
 function convertMiliyar(arr, position){
     if(typeof position !== 'number'){
@@ -196,7 +211,6 @@ function convertRibu(arr, position){
     startIndexPosition += 1;
     return word;
 }
-
 function wordMoneyCurrency(arr, money){
     if(typeof arr === 'undefined' || typeof arr !== 'object'){
         return
@@ -205,8 +219,20 @@ function wordMoneyCurrency(arr, money){
         return 
     }
     let word = '';
-
-    const length = arr.length;    
+	/**
+	 * Jika Array Ada 4 Maka Miliyar
+	 * Jika Array Ada 3 Maka Juta
+	 * Jika Array Ada 2 Maka Ribu
+	 * Jika Ingin Penambahan Bilangan Seperti Triliyun
+	 * Cukup Menambah Perkondision IF 
+	 * Dengan Length >= 5 dan satu function yang sama
+	 * Dengan function lain (convertJuta, convertMiliyar dan lain)
+	 */
+    const length = arr.length;
+	console.log(arr);
+	if(length >= 5){
+		word += convertTrilyun(arr, startIndexPosition);
+	}
     if(length >= 4){
         word += convertMiliyar(arr, startIndexPosition);
         // console.log(word)
@@ -214,45 +240,60 @@ function wordMoneyCurrency(arr, money){
 
     if(length >= 3){
         word += convertJuta(arr, startIndexPosition);
-        console.log(word)
+        // console.log(word)
     }
 
     if(length >= 2){
         word += convertRibu(arr, startIndexPosition)
-        console.log(word)
+        // console.log(word)
     }
     // if(arr)
     word += readRatusan(arr[startIndexPosition]);
-    console.log(word)
+	return word;
+}
+/**
+ * Mengubah String (Uang) Menjadi
+ * Array Multidimesional
+ * 
+ * @param {string} money
+ * @return {string} Word
+ */
+function convertMoney(/*string*/ money){
+	if(typeof money !== 'string'){
+		return;
+	}
+	
+	const currency = money.split('').reverse();
+	let arr = [];
+	let temp = [];
+	startIndexPosition = 0;
+
+	currency.forEach((val, ind) => {
+		temp.push(val)
+		if((ind + 1) % 3 == 0){
+			arr.push(temp);
+			temp = [];
+		}
+	});
+
+	if(temp.length != 0){
+		arr.push(temp);
+		temp = [];
+	}
+
+	const arrRev = arr.reverse()
+	let newArrRev = [];
+	arrRev.forEach(val => {
+		newArrRev.push(reverseArrayInPlace(val));
+	})
+	return wordMoneyCurrency(newArrRev, currency);
 }
 
-// Test Case 
-const money = '1400100'.split('').reverse();
-const arr = [];
-let temp = [];
-
-money.forEach((val, ind) => {
-    temp.push(val)
-    // console.log(ind + 1);
-    if((ind + 1) % 3 == 0){
-        arr.push(temp);
-        temp = [];
-    }
-})
-
-// Push Sisa Temp
-if(temp.length != 0){
-    arr.push(temp);
-    temp = [];
+function reverseArrayInPlace(array) {
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    let old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
+  }
+  return array;
 }
-
-const arrRev = arr.reverse()
-let newArrRev = [];
-arrRev.forEach(val => {
-    newArrRev.push(val.reverse())
-})
-
-wordMoneyCurrency(newArrRev, money);
-
-// console.log(arr);
-
